77bebe6e6ff797e3e77e622b26dbb15848d3d06b
Bradley BARBIER <bradley.barbier@outlook.fr>
2021-12-13 16:31:41 +0100

    First commit
    Création des différents fichiers qui sera utilisés pendant le projet

---
+++ assets/components/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#">Parc informatique du Lycée Frédéric Chopin</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="navbar-nav mx-auto">
+            <a href="#" class="nav-link active">Accueil</a>
+            <a href="#" class="nav-link">Notre equipe</a>
+        </div>
+        <div class="navbar-nav ml-auto">
+            <a href='/vues/login.php' class="btn btn-dark mr-3 ml-5">Connexion</a>
+            <a href='/vues/register.php' class="btn btn-light">Enregistement</a>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$title = 'Accueil - Parc Informatique';
+require './assets/components/header.php';
+require './assets/components/navbar.php';
+?>
+
+
+<div class="container mt-4">
+    <h2 class="text-center">Bienvenue sur le parc informatique du lycée Frédéric Chopin à Nancy.</h2>
+    
+    <p class="mt-4 text-justify">Ce parc informatique permet au professeur ou administrateur réseaux de pouvoir visualiser les différents appareils des étudiants.</p>
+    <p>Les appareils pris en compte sont par exemple :
+        <ul>
+            <li>Les ordinateurs</li>
+            <li>Les ordinateurs portables</li>
+            <li>Les téléphones</li>
+            <li>Les tablettes</li>
+        </ul>
+    </p>
+
+    <p></p>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<?php
+require './assets/components/footer.php';
+?>
