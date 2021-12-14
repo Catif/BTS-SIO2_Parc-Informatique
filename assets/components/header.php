1d28c36bd6d309ca7bef000e8f0be87df092c46e
Bradley BARBIER <bradley.barbier@outlook.fr>
2021-12-14 13:19:51 +0100

    MàJ de Bootstrap
    Bootstrap 4.3.1 vers Bootstrap 5.1.3

--- assets/components/header.php
+++ assets/components/header.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?></title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="/assets/css/style.css">
     </head>
     <body>
