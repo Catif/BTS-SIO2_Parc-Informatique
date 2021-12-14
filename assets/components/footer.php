8bb101bb501be5226c5b8accf5dacd79a3a26435
Bradley BARBIER <bradley.barbier@outlook.fr>
2021-12-14 13:21:30 +0100

    Ajout du fichier JS de Bootstrap

--- assets/components/footer.php
+++ assets/components/footer.php
@@ -1,3 +1,3 @@
-</div> <!-- Fin du div.container (./assets/components/header.php) -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
